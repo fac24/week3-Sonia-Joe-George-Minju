59249f39c94260d342e1bb7e521453d7b65afd51
glombort <george.lambert99@gmail.com>
2022-05-25 15:38:35 +0100

    Update connection.js
    Create connection to the database
    
    Co-Authored-By: Sonia <82713219+sonianb@users.noreply.github.com>
    Co-Authored-By: Joe <94115299+joe-dev-public@users.noreply.github.com>
    Co-Authored-By: Minju Kim <minju25kim@gmail.com>

---
+++ database/connection.js
@@ -0,0 +1,18 @@
+const pg = require("pg");
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
+const options = {
+  connectionString: process.env.DATABASE_URL,
+  // We shouldn't need this if we're using a local pg db for dev and testing.
+  // (When the app is deployed, this setting shouldn't be required for Heroku to connect to its own Postgres db.)
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
+};
+
+const db = new pg.Pool(options);
+
+module.exports = db;
