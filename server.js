f32319d088d8c73eb596076d9bb37477fae04f6e
glombort <george.lambert99@gmail.com>
2022-05-25 17:05:27 +0100

    Update server.js
    Added aspects for our server file
    
    Co-Authored-By: Sonia <82713219+sonianb@users.noreply.github.com>
    Co-Authored-By: Joe <94115299+joe-dev-public@users.noreply.github.com>
    Co-Authored-By: Minju Kim <minju25kim@gmail.com>

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const home = require("./routes/home.js");
+const login = require("./routes/login.js");
+const signUp = require("./routes/signUp.js");
 
 const server = express();
 
 //Static files e.g. css, images...
 const staticHandler = express.static("public");
 server.use(staticHandler);
+const bodyHandler = express.urlencoded({ extended: false });
+server.use(bodyHandler);
 
-const bodyHandler = express.urlencoded({ extended:false });
+server.get("/", home.get);
 
-server.get("/", home.get)
+server.get("/login", login.get);
+server.post("/login", login.post);
 
-
+server.get("/sign-up", signUp.get);
+server.post("/sign-up", signUp.post);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
