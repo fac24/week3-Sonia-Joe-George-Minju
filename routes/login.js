031fb6e3b5841ee8be7714f85977ff4571ec4344
glombort <george.lambert99@gmail.com>
2022-05-26 15:11:58 +0100

    verify login.js
    Verify now works for login
    
    Co-Authored-By: Sonia <82713219+sonianb@users.noreply.github.com>
    Co-Authored-By: Joe <94115299+joe-dev-public@users.noreply.github.com>
    Co-Authored-By: Minju Kim <minju25kim@gmail.com>

--- routes/login.js
+++ routes/login.js
@@ -12,6 +12,7 @@ function get(request, response) {
       <label for="password">Password</label>
       <input type="password" id="password" name="password">
       <button>Log in</button>
+      <a href="/">Back to Home</a>
     </form>
   `
   );
@@ -27,13 +28,27 @@ function post(request, response) {
     .verifyUser(username, password)
     // If the password matches, then create a new, unique session ID for
     // the authenticated user and store it in the database (both done in auth.js)
-    .then((user) => auth.createSession(user.username))
+    .then((verifyResponse) => {
+      // What the above promise returns is either "false" (boolean) if the username/pass don't match
+      // or the user object, if the username and pass do match. "verifyResponse" isn't the best name for this :)
+      //console.log("///", verifyResponse);
+      if (verifyResponse === false) {
+        // Username or password didn't match, so show the user an error
+        throw new Error();
+      } else {
+        return auth.createSession(verifyResponse);
+      }
+    })
     .then((sid) => {
       // And finally, send the user a signed cookie with this session ID:
       //console.log(sid);
       response.cookie("sid", sid, auth.COOKIE_OPTIONS);
       // The user is now logged-in! So redirect them to .. somewhere (probably the home page).
-      response.redirect("/posts");
+      response.redirect("/");
+    })
+    .catch(() => {
+      //console.log(error);
+      response.status(401).send(`<h1>Oops, something went wrong!</h1>`);
     });
 }
 
