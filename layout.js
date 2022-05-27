aa3860f4367347bbdd87ef4f88d631cdb0301d42
sonianb <sonia.botsarova@gmail.com>
2022-05-27 11:31:01 +0100

    import font

--- layout.js
+++ layout.js
@@ -6,13 +6,12 @@ function layout(title, content) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Hind&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Moo+Lah+Lah&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Kalam&display=swap" rel="stylesheet">
   <title>${title}</title>
-  <link rel="stylesheet" href="../public/style.css" type="text/css">
+  <link rel="stylesheet" href="/styles.css" type="text/css">
 </head>
 <body>
-  <h1>Moo Lah Lah</h1>
-  <p>Making Cows Happy since 25/06, 12.25 p.m. </p>
   ${content}
 </body>
 </html>
